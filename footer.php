896e82d32dbaf32e5f3a3168e641e426a457001e
Rits-theDeveloper <ritesh.mestry.naresh@gmail.com>
2022-05-25 18:04:33 +0530

    new providers page

--- footer.php
+++ footer.php
@@ -28,142 +28,129 @@
     align-content: center;
 }
   }
-</style>
 
-
-
-<div class="footer-header__background">
-         <div class="container">
-            <ul class="footer-header ">
-               <li class="footer-header__item  footer-header__item--104"><a href="http://texasspecialtyclinic.com/about-us/">About Us</a></li>
-               <li class="footer-header__item  footer-header__item--105"><a href="http://texasspecialtyclinic.com/request-an-appointment/">Request an appointment</a></li>
-               
-               <li class="footer-header__item  footer-header__item--106"><a href="http://texasspecialtyclinic.com/services/">Services</a></li>
-               <li class="footer-header__item  footer-header__item--3513"><a href="http://texasspecialtyclinic.com/contact/">Contact us</a></li>
-            </ul>
-         </div>
-      </div>
-      <footer class="footer">
-         <div class="container flexComponent flexComponent--row">
-            <div class="footer__col">
-              
-               <div class="footer__block flexComponent flexComponent--row">
-                <img src="http://texasspecialtyclinic.com/assets/images/TSC-logo.png" class="footer__ctaImage">
-                  <div class="footer__col">
-
-                     <p class="footer__textarea">
-                        <!-- Texas Specialty Clinic<br>
-2700 W. Pleasant Run Rd.<br>
-Lancaster, TX 75146   -->
-                        <b>Lancaster Location:</b><br>
-                    2700 W. Pleasant Run Rd.<br>
-                    Lancaster, TX 75146<br><br>
-
-
-
-                    <!-- <b>MLK Location: </b><br>
-                    2949 Martin Luther King, Jr. Blvd.<br>
-                    #3 Dallas, TX 75215    
-                     </p> -->
-
-                     <a href="mailto:info@texasspecialtyclinic.com">info@TexasSpecialtyClinic.com</a> </br>
-                        Phone: <a href="tel:(888) 871-1883">(888) 871-1883</a><br>
-                        Fax: (888) 900-4530
-
-
-                  </div>
-
-
-                  <img src="http://texasspecialtyclinic.com/assets/images/TSC-logo.png" class="footer__ctaImage">
-                  <div class="footer__col">
-                    
-                     <p class="footer__textarea">
-
-                      <b>Oak Lawn Location: </b><br>
-                    2603 Oaklawn Avenue, Suite 101<br>
-                    Dallas, TX  75219   <br>
-                    <b><a href="https://www.tscoaklawn.com/" target="_blank" style="text-decoration: underline;">Oak Lawn Location</a></b> 
-                    <!-- <a href="https://www.tscoaklawn.com/" style="color: blue;text-decoration: underline;">www.tscoaklawn.com</a> -->
-
-                        <!-- <a href="mailto:info@texasspecialtyclinic.com">info@TexasSpecialtyClinic.com</a> </br>
-                        Phone: <a href="tel:(888) 871-1883">(888) 871-1883</a><br>
-                        Fax:<a href="(888) 900-4530"> (888) 900-4530</a> -->
-
-
-                        <ul class="social-icons social-icons--pink display-flex"><br class="desk-vis">
-                        <!-- <li class="social-icons__item">
-                           <a href="https://twitter.com/TxSpecialty" target="_blank" class="social-icons__link"><i class="fab fa-twitter"></i></a>
-                        </li><br class="desk-vis"><br class="desk-vis"> -->
-                        <li class="social-icons__item">
-                           <a href="https://www.facebook.com/txspecialty" target="_blank" class="social-icons__link"><i class="fab fa-facebook"></i></a>
-                        </li><!-- <br class="desk-vis"><br class="desk-vis"> -->
-                       <!--  <li class="social-icons__item">
-                           <a href="" target="_blank" class="social-icons__link"><i class="fab fa-youtube"></i></a>
-                        </li> -->
-                        <!-- <li class="social-icons__item">
-                           <a href="https://www.instagram.com/txspecialty/" target="_blank" class="social-icons__link"><i class="fab fa-instagram"></i></a>
-                        </li> -->
-                     </ul>
-
-
-
-                     </p>
-                     
-                  </div>
-                  <style type="text/css">
                     @media (max-width: 600px){
                       .desk-vis{
                         display: none;
                       }
                     }
                   </style>
-
-                  <!-- <ul class="social-icons social-icons--pink"><br class="desk-vis">
-                        <li class="social-icons__item">
-                           <a href="https://twitter.com/TxSpecialty" target="_blank" class="social-icons__link"><i class="fab fa-twitter"></i></a>
-                        </li><br class="desk-vis"><br class="desk-vis">
-                        <li class="social-icons__item">
-                           <a href="https://www.facebook.com/txspecialty" target="_blank" class="social-icons__link"><i class="fab fa-facebook"></i></a>
-                        </li><br class="desk-vis"><br class="desk-vis">
-                        <li class="social-icons__item">
-                           <a href="https://www.instagram.com/txspecialty/" target="_blank" class="social-icons__link"><i class="fab fa-instagram"></i></a>
-                        </li>
-                     </ul> -->
-               </div>
-             
-            </div>
-            <div class="footer__col" id="newsletter-box">
-               
-               <div class="footer__block flexComponent flexComponent--row footer__block--right">
-                  
-                  <!-- <img src="http://texasspecialtyclinic.com/assets/images/footer.png" class="footer__ctaImage"> -->
-
-                  <div class="footer__signup">
-                     <h2 class="footer__signupTitle">News & Updates</h2>
-                     <p class="footer__signupText">Subscribe to our newsletter for all things</p>
-                     <form action="" method="post" id="form2" name="mc-embedded-subscribe-form" class="validate" style="position: relative;">
-                        <input required type="email" value="" name="EMAIL" class="required footer__signupInput" id="mce-EMAIL" placeholder="Email Address">
-                        <button class="footer_signupButton button button--pink" id="arrows"><i class="fas fa-angle-right"></i></button><div id="submsg" style="display:none;margin-top:5px;color:white;">Subscribed to our newsletter</div>
-                     </form>
-                     <img src="http://texasspecialtyclinic.com/assets/images/logo.png" class="footer__ctaImage" style="width: 100%;height: auto;">
-                  </div>
-               </div>
-            </div>
-         </div>
-      </footer>
-      <div class="company-footer">
-         <div class="container company-footer__container">
-          <!-- <ul class="company-footer__list">
-      <li class="company-footer__listItem"></li>
-      <li class="company-footer__listItem"></li>
-      <li class="company-footer__listItem"></li>
-    </ul> -->
-            <ul class="company-footer__list" style="width: 100%;">
-               <li class="company-footer__listItem">Web designed by <a href="https://ibridgedigital.com/" target="_blank" class="company-footer__listLink">iBridge Digital</a></li>
-            </ul>
-         </div>
+<div class="footer-header__background">
+  <div class="container">
+    <ul class="footer-header ">
+      <li class="footer-header__item  footer-header__item--104">
+        <a href="http://texasspecialtyclinic.com/about-us/">About Us</a>
+      </li>
+      <li class="footer-header__item  footer-header__item--105">
+        <a href="http://texasspecialtyclinic.com/request-an-appointment/">Request an appointment</a>
+      </li>
+      <li class="footer-header__item  footer-header__item--106">
+        <a href="http://texasspecialtyclinic.com/services/">Services</a>
+      </li>
+      <li class="footer-header__item  footer-header__item--3513">
+        <a href="http://texasspecialtyclinic.com/contact/">Contact us</a>
+      </li>
+    </ul>
+  </div>
+</div>
+<footer class="footer">
+  <div class="container flexComponent flexComponent--row">
+    <div class="footer__col">
+      <div class="footer__block flexComponent flexComponent--row">
+        <img src="http://texasspecialtyclinic.com/assets/images/TSC-logo.png" class="footer__ctaImage">
+        <div class="footer__col">
+          <p class="footer__textarea">
+            <b>Lancaster Location:</b>
+            <br> 2700 W. Pleasant Run Rd, Suite 340, <br>Lancaster, TX 75146<br>
+            <b>
+              <a href="https://goo.gl/maps/69NhBcNw9eEPUurS9" target="_blank" style="text-decoration: underline;">View Map</a>
+            </b>
+          </p>
+        </div>
+        <img src="http://texasspecialtyclinic.com/assets/images/TSC-logo.png" class="footer__ctaImage">
+        <div class="footer__col">
+          <p class="footer__textarea">
+            <b>Oaklawn Location: </b>
+            <br> 2603 Oak Lawn Ave., Suite.101 <br>Dallas, TX 75219<br>
+            <b>
+              <a href="https://goo.gl/maps/FgYD1pmFa9aukNj68" target="_blank" style="text-decoration: underline;">View Map</a>
+            </b>
+          </p>
+        </div>
+        <img src="http://texasspecialtyclinic.com/assets/images/TSC-logo.png" class="footer__ctaImage">
+        <div class="footer__col">
+          <p class="footer__textarea">
+            <b>Hillsboro Location: </b>
+            <br> 117 Jane Lane Hillsboro, <br>TX 76645<br>
+            <b>
+              <a href="https://goo.gl/maps/24ui9nTbGpboPE7n6" target="_blank" style="text-decoration: underline;">View Map</a>
+            </b>
+          </p>
+        </div>
       </div>
-
+      <div class="footer__block flexComponent flexComponent--row">
+        <img src="http://texasspecialtyclinic.com/assets/images/TSC-logo.png" class="footer__ctaImage">
+        <div class="footer__col">
+          <p class="footer__textarea">
+            <b>Midland Location:</b>
+            <br> 1304 W Texas Avenue, <br>Midland, TX 79701<br>
+            <b>
+              <a href="https://goo.gl/maps/nSaDPLQQWvc38P489" target="_blank" style="text-decoration: underline;">View Map</a>
+            </b>
+          </p>
+        </div>
+        <img src="http://texasspecialtyclinic.com/assets/images/TSC-logo.png" class="footer__ctaImage">
+        <div class="footer__col">
+          <p class="footer__textarea">
+            <b>Grand Prairie Location: </b>
+            <br> 517 N. Carrier Parkway, <br>Grand Prairie, TX 75050<br>
+            <b>
+              <a href="https://goo.gl/maps/eEBYwMxLwi9DojtK6" target="_blank" style="text-decoration: underline;">View Map</a>
+            </b>
+          </p>
+        </div>
+        <img src="http://texasspecialtyclinic.com/assets/images/TSC-logo.png" class="footer__ctaImage">
+        <div class="footer__col">
+          <p class="footer__textarea">
+            <b>Farmers Branch Location: </b>
+            <br> 13988 Diplomat Drive, Suite 100 <br>Farmers Branch, TX 75234<br>
+            <b>
+              <a href="https://goo.gl/maps/5dh3oBbQSK1syQhL6" target="_blank" style="text-decoration: underline;">View Map</a>
+            </b>
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="footer__col" id="newsletter-box">
+      <div class="footer__block flexComponent flexComponent--row footer__block--right">
+        <div class="footer__signup">
+          <h2 class="footer__signupTitle">Contact us</h2>
+          <p class="footer__signupText">
+            <a href="mailto:info@texasspecialtyclinic.com" style="color: #464646">info@TexasSpecialtyClinic.com</a>
+            </br> Phone: <a href="tel:(888) 871-1883" style="color: #464646">(888) 871-1883</a>
+            <br> Fax: (888) 900-4530
+          </p>
+          <ul class="social-icons social-icons--pink display-flex">
+            <br class="desk-vis">
+            <li class="social-icons__item">
+              <a href="https://www.facebook.com/txspecialty" target="_blank" class="social-icons__link">
+                <i class="fab fa-facebook"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+<div class="company-footer">
+  <div class="container company-footer__container">
+    <ul class="company-footer__list" style="width: 100%;">
+      <li class="company-footer__listItem">Web designed by <a href="https://ibridgedigital.com/" target="_blank" class="company-footer__listLink">iBridge Digital</a>
+      </li>
+    </ul>
+  </div>
+</div>
 <script>
         jQuery(document).ready(function($) {
                                   
